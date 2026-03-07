e98fe1e862f80b000a216682a39d24b0e35226e9
mkellermeier <michaelkellermeier@gmx.de>
2026-03-07 21:05:01 +0100

    chore: use default GitHub Pages URL for testing

--- astro.config.ts
+++ astro.config.ts
@@ -22,7 +22,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  site: 'https://tcbuehlertal.de',
+  site: 'https://mkellermeier.github.io',
+  base: '/tcb',
   output: 'static',
 
   integrations: [
